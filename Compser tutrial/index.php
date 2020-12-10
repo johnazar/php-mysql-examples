778b3a7699c5788502d680cad47cfc7f96a262aa
John Azar <eng.johnazar@gmail.com>
2020-12-10 12:21:40 +0300

    composer

--- Compser tutrial/index.php
+++ Compser tutrial/index.php
@@ -1,5 +1,8 @@
 <?php 
-require "vendor/autoload.php";
+require __DIR__ . '/vendor/autoload.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
 
 $myArry = array(
     'person' => array(
@@ -12,5 +15,18 @@ $firstname = data_get($myArry,'person.f_name');
 //$firstname = $myArry['person']['f_name'];
 
 echo $firstname;
+
+
+// Create the logger
+$logger = new Logger('my_logger');
+// Now add some handlers
+$logger->pushHandler(new StreamHandler('my_log.log', Logger::DEBUG));
+$logger->pushHandler(new FirePHPHandler());
+// You can now use your logger
+$logger->info('My logger is now ready');
+// You can now use your logger
+
+echo $logger;
+
 
 ?>

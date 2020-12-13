8de8b94a78cf2a834d0518a5412aba7a6dee2db3
John Azar <eng.johnazar@gmail.com>
2020-12-13 10:31:07 +0300

    php crud versions

--- PHP-Football/templates/frontpage.php
+++ PHP-Football/templates/frontpage.php
@@ -33,13 +33,13 @@
 </div>
 
 <div class="container d-flex justify-content-around flex-wrap">
-<?php foreach($players as $player):?>
+<?php foreach($players as $key=>$player):?>
         <!-- Three columns of text below the carousel -->
           <div class="col-md-4">
               <div class="card mb-2" style="width: 18rem;">
                 <img class="card-img-top" src="<?php echo $player->img;?>" alt="Card image cap">
                 <div c  lass="card-body">
-                  <h5 class="card-title"><?php echo $player->firstname;?></h5>
+                  <h5 class="card-title"><?php echo $key + 1 ."-".$player->firstname;?></h5>
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
                 <ul class="list-group list-group-flush">
